a08881624f57ca5cbdc2cf4d3dccbf44143d1117
Caio Uehara <caioueharamartins@gmail.com>
2023-10-02 19:53:43 -0300

    Add Flask

--- source/main.py
+++ source/main.py
@@ -1,5 +1,16 @@
+import flask
 from connectNewsAPI import connectNewsApi
-from saveData import saveData, printData
+from saveData import saveData
 
-saveData(connectNewsApi())
-printData()
+
+app = flask.Flask(__name__)
+
+@app.route("/updateNews", methods=["GET"])
+def handle_webhook():
+    data = connectNewsApi()
+    saveData(data)
+    
+    return "OK"
+
+if __name__ == "__main__":
+    app.run()
